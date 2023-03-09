b5fec1719b88400057e3740bfec85322eeb45233
Éverson Andrade <andrade.everson@outlook.com>
2023-03-09 14:46:42 -0400

    exercicios

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 import express, { Request, Response } from 'express'
 import cors from 'cors'
 import { db } from './database/knex'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
 
-app.use(express.json())
 app.use(cors())
+app.use(express.json())
 
-app.listen(3003, () => {
-    console.log("Servidor rodando na porta 3003")
+app.listen(Number(process.env.PORT), () => {
+    console.log(`Servidor rodando na porta ${Number(process.env.PORT)}`)
 })
 
 app.get('/ping', (req: Request, res: Response) => {
